bc8688e45303ef50183230ebf0fd1682e2dd058a
karlo <karlomr@gmail.com>
2021-06-12 17:12:14 -0300

    id with uuid

--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,8 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use("/categories",categoriesRoutes);
 
-// app.get('/', (request, response) => {
-//     return response.json({message: "Hello World!"});
-// })
-
-// app.post("/courses", (request, response) => {
-//     const { name } = request.body;
-//     return response.json({ name });
-// })
 
 app.listen(3333, () => console.log("Server is running"));
 

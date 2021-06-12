085072d73bb3644e743551dbbc60de0fa60b0f25
karlo <karlomr@gmail.com>
2021-06-12 18:10:34 -0300

    category list

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,11 +1,9 @@
 import { Category } from "../model/Category";
 
-interface ICreateCategoryDTO{
+interface ICreateCategoryDTO {
     name: string;
     description: string;
 }
-
-
 class CategoriesRepository {
 
     private categories: Category[];
@@ -14,15 +12,21 @@ class CategoriesRepository {
         this.categories = [];
     }
 
-    create({ description, name } :ICreateCategoryDTO): void {
+    create({ description, name }: ICreateCategoryDTO): void {
         const category = new Category();
 
-        Object.assign(category, { 
-            name, 
+        Object.assign(category, {
+            name,
             description,
             created_at: new Date(),
-         });
+        });
+
+        this.categories.push(category);
+    }
+
+    list(): Category[] {
+        return this.categories;
     }
 }
 
-export { CategoriesRepository}
+export { CategoriesRepository };
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,5 @@ app.use(express.json());
 
 app.use("/categories",categoriesRoutes);
 
-
 app.listen(3333, () => console.log("Server is running"));
 

644d20055ed1c4996c3a29e89f61b7bfeb302780
karlo <karlomr@gmail.com>
2021-06-12 17:37:02 -0300

    category typing

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { v4 as uuidV4 } from 'uuid'; 
+import { Category } from '../model/Category';
 
 
 const categoriesRoutes = Router();
@@ -11,15 +12,16 @@ const categories = [];
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const category = {
-        id: uuidV4(),
-        name,
-        description
-    };
+    const category = new Category();
+    Object.assign(category, { 
+        name, 
+        description,
+        created_at: new Date(),
+     });
 
     categories.push(category);
 
-    return response.status(201).send("Category Created!");
+    return response.status(201).send({category});
 });
 
 export { categoriesRoutes };
